aa1063a86efd521be77c697061b3ce8836e6d695
Jakob Winn <jakobjwinn@gmail.com>
2023-05-31 20:47:04 -0600

    Initial program

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment simpleAssignment = new Assignment("Nicholas Johnson", "Science");
+        Console.WriteLine(simpleAssignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("Ashton Howard", "Geometry", "10.9", "27-38");
+        Console.WriteLine(Environment.NewLine + mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Stephen Caringer", "US History", "The State of our Nation");
+        Console.WriteLine(Environment.NewLine + writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInfo());
     }
 }
