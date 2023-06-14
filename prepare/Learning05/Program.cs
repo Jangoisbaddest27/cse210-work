34ed38a7c4319f253232dbc5b523b00be2b769c1
Jakob Winn <jakobjwinn@gmail.com>
2023-06-14 14:11:16 -0600

    Original draft.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square1 = new Square("blue", 5);
+        // Console.WriteLine(square1.GetColor());
+        // Console.WriteLine(square1.GetArea());
+
+        Rectangle rect1 = new Rectangle("red", 5, 3);
+        // Console.WriteLine(rect1.GetColor());
+        // Console.WriteLine(rect1.GetArea());
+
+        Circle circle1 = new Circle("orange", 5);
+        // Console.WriteLine(circle1.GetColor());
+        // Console.WriteLine(circle1.GetArea());
+
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(square1);
+        shapes.Add(rect1);
+        shapes.Add(circle1);
+
+        foreach (Shape s in shapes)
+        {
+            Console.WriteLine(s.GetColor());
+            Console.WriteLine(s.GetArea());
+        }
     }
 }
